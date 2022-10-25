e5c90e72d0a2033a86c3fec4c87b5601b533799f
Derja12 <derekjacketta@gmail.com>
2022-10-24 21:46:52 -0600

    saves images

--- example.py
+++ example.py
@@ -2,9 +2,10 @@ import time
 import keras_cv
 from tensorflow import keras
 import matplotlib.pyplot as plt
+from PIL import Image
 
-model = keras_cv.models.StableDiffusion(img_width=128, img_height=128)
-images = model.text_to_image("utah jazz win the championship jordan clarkson celebrates with kelly olynyk", batch_size=3)
+model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
+images = model.text_to_image("basketball flies into the sun, realistic", batch_size=2)
 
 def plot_images(images):
     plt.figure(figsize=(20, 20))
@@ -12,6 +13,7 @@ def plot_images(images):
         ax = plt.subplot(1, len(images), i + 1)
         plt.imshow(images[i])
         plt.axis("off")
+        Image.fromarray(images[i]).save("test"+str(i)+".png")
 
 
 plot_images(images)
